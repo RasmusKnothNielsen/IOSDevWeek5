1bb2d74d8ccda093641703b859760aa75d89cd86
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-02-07 10:30:18 +0100

    Initial Commit

---
+++ HelloName/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloName
+//
+//  Created by Rasmus Knoth Nielsen on 07/02/2020.
+//  Copyright © 2020 Rasmus Knoth Nielsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
